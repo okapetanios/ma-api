598c3c259c34643f2f69eea9f51ce7ef63e02e5c
Nicole Yelgate <Nicole Yelgate>
2020-03-21 16:46:41 -0600

    Added usermodel

---
+++ src/main/java/com/example/maapi/models/User.java
@@ -0,0 +1,44 @@
+package com.example.maapi.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    private String username;
+    private String password;
+    private String fname;
+    private String lname;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+}
